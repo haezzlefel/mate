24163268c4bd3945580f24dd2359e2aa75c53ae5
haezzlefel <zerrazozila@gmail.com>
2023-10-11 19:42:22 +0800

    new file:   css/stylef.css 	new file:   friendlist.html 	new file:   friendlist2.html 	new file:   friendlist3.html 	deleted:    images/Data Privacy and Consumer Protectio.pdf 	new file:   images/cute.jpg 	new file:   images/cute2.jpg 	new file:   images/cute3.jpg 	new file:   images/cute4.jpg 	new file:   images/cute5.jpg 	new file:   images/cute6.jpg 	new file:   images/fel.jpg 	new file:   images/hannah.jpg 	new file:   images/p1.webp 	new file:   images/p10.jpeg 	new file:   images/p11.webp 	new file:   images/p12.jpg 	new file:   images/p13.jpg 	new file:   images/p14.jpg 	new file:   images/p15.jpg 	new file:   images/p16.jpg 	new file:   images/p17.jpg 	new file:   images/p18.jpg 	new file:   images/p19.jpg 	new file:   images/p2.png 	new file:   images/p20.jpg 	new file:   images/p21.jpg 	new file:   images/p22.jpg 	new file:   images/p23.jpg 	new file:   images/p24.jpg 	new file:   images/p3.jpeg 	new file:   images/p4.jpeg 	new file:   images/p5.jpeg 	new file:   images/p6.jpeg 	new file:   images/p7.jpeg 	new file:   images/p8.webp 	new file:   images/p9.jpeg 	new file:   images/p9.webp 	modified:   index.html 	new file:   mate js/friendlist.js

---
+++ mate js/friendlist.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// API URL
+const HOST_API_URL = "#";
+const INDEX_API_URL = HOST_API_URL + "/api/v1/users";
+
+
+const friends = [];
+
+
+const friendList = document.querySelector("#friend-list-container");
+
+
+function renderFriendList(friends) {
+  
+  if (!friends || !friends.length) {
+    return;
+  }
+  
+  let rawHTML = "";
+  
+  friends.forEach((friend) => {
+    //  template 
+    rawHTML += `
+                <div class="col-sm-3">
+                    <!-- Cards -->
+                    <div class="friend-cards border-0 m-3" style="width: 16rem;">
+                        <!-- Image -->
+                        <img class="card-img-top border rounded-circle"
+                            src="${friend.avatar}" alt="friend-image"
+                            data-bs-toggle="modal" 
+                            data-bs-target="#friendList" 
+                            class="img-fluid"
+                            data-id="${friend.id}">
+                        <!-- Title -->
+                        <div class="card-body m-1 p-0">
+                            <h5 class="card-title text-center m-0">
+                            ${friend.name} ${friend.surname}
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+        `;
+    // 
+    friendList.innerHTML = rawHTML;
+  });
+}
+
+//  Modal
+function displayFriendDetail(id) {
+  //  modal 
+  const modalTitle = document.querySelector("#modal-title");
+  const modalSrc = document.querySelector("#modal-src");
+  const modalEmail = document.querySelector("#modal-email");
+  const modalGender = document.querySelector("#modal-gender");
+  const modalAge = document.querySelector("#modal-age");
+  const modalRegion = document.querySelector("#modal-region");
+  const modalBirthday = document.querySelector("#modal-birthday");
+
+  //  modal  user 
+  modalTitle.textContent = ``;
+  modalSrc.src = ``;
+  modalEmail.textContent = ``;
+  modalGender.textContent = ``;
+  modalAge.textContent = ``;
+  modalRegion.textContent = ``;
+  modalBirthday.textContent = ``;
+
+  //  ID 
+  axios
+    .get(INDEX_API_URL + `/${id}`)
+    .then(function (response) {
+      // API
+      const data = response.data;
+      //  modal 
+      modalTitle.textContent = `${data.name} ${data.surname}`;
+      modalSrc.src = `${data.avatar}`;
+      modalEmail.textContent = `email:${data.email}`;
+      modalGender.textContent = `gender:${data.gender}`;
+      modalAge.textContent = `age:${data.age}`;
+      modalRegion.textContent = `region:${data.region}`;
+      modalBirthday.textContent = `birthday:${data.birthday}`;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
+
+// API 
+axios
+  .get(INDEX_API_URL)
+  .then(function (response) {
+
+    friends.push(...response.data.results);
+
+    renderFriendList(friends);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+
+friendList.addEventListener("click", function clickOnImage(event) {
+
+  if (event.target.classList.contains("card-img-top")) {
+
+    displayFriendDetail(Number(event.target.dataset.id));
+  }
+});
